c57fe20c9dfdf333c2440698f3ae070c2129af20
Marty McLaughlin <martymclaugh@gmail.com>
2016-09-15 11:49:14 -0700

    add devise and configure mailers for forgotten password

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,16 @@ module MenuPricer
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'mail.google.com',
+      user_name:            'marmapps@gmail.com',
+      password:             'marmappspass',
+      authentication:       'login',
+      enable_starttls_auto: true
+    }
+
   end
 end
